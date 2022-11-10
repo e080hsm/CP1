544f2f35c2e0ec598d28a9254f07f39f724580f7
e080hsm <e080hsm@gmail.com>
2022-11-10 13:49:05 +0900

    add message for worknet_scrapper and little modify

--- scrapper/worknet_api.py
+++ scrapper/worknet_api.py
@@ -8,11 +8,20 @@ import json
 class worknet_scrapper:
     ADDRESS = config('WORKNET')
     AUTHKEY = config('AUTHKEY')
-    def get_address(base_url:str, para:dict):
-        para_list = [base_url] + [f'{key}={val}' for key, val in para.items()]
+    def get_address(para:dict):
+        '''
+        받아온 파라미터에 맞춰 웹 주소를 반환한다.
+        para: 파라미터 리스트
+        '''
+        ADDRESS = f'{worknet_scrapper.ADDRESS}?authKey={worknet_scrapper.AUTHKEY}'
+        para_list = [ADDRESS] + [f'{key}={val}' for key, val in para.items() if val != None]
         return '&'.join(para_list)
 
     def get_json(url:str):
+        '''
+        워크넷 open API 주소를 requests를 이용해 가져온다.
+        url: 워크넷 openAPI 웹 주소
+        '''
         response = requests.get(url)
 
         if response.status_code != 200:
@@ -25,10 +34,17 @@ class worknet_scrapper:
         self.api_list = []
         self.api_detail = []
 
-    def scrap_api_list(self, startPage=1, endPage=1, display=10, occupation='', inplace=False):
+    def scrap_api_list(self, startPage=1, endPage=1, display=10, occupation=None, inplace=False):
+        '''
+        워크넷 오픈API에서 리스트 데이터를 받아온다.
+        startPage: 검색 시작 페이지.
+        endPage: 검색 종료 페이지. startPage~endPage까지 가져온다.
+        display:한 페이지에 표시되는 명단 개수. 기본 및 최소 10, 최대 100
+        occupation: 직업 코드. 직업코드는 직종코드.xls 참조.
+        inplace: 종말점 설정. True면 None 반환.
+        '''
         CALL_TP ='L'
         RET_TP='XML'
-        ADDRESS = f'{worknet_scrapper.ADDRESS}?authKey={worknet_scrapper.AUTHKEY}'
 
         for page in range(startPage, endPage + 1):
             list_para={
@@ -39,10 +55,11 @@ class worknet_scrapper:
                 'occupation':occupation
             }
 
-            # get list from worknet api
-            address_list  = worknet_scrapper.get_address(ADDRESS, list_para)
+            # make addres for API
+            address_list  = worknet_scrapper.get_address(list_para)
             
             try:
+                # get list from worknet api
                 self.api_list.extend(worknet_scrapper.get_json(address_list)['wantedRoot']['wanted'])
                 time.sleep(1)
             except ConnectionRefusedError:
@@ -50,9 +67,12 @@ class worknet_scrapper:
         return None if inplace else self
     
     def scrap_api_detail(self, inplace=False):
+        '''
+        scrap_api_list를 이용해 워크넷에서 받아온 API리스트에서 세부 정보를 추가적으로 찾아낸다.
+        inplace: 종말점 설정. True면 None 반환.
+        '''
         CALL_TP = 'D'
         RET_TP='XML'
-        ADDRESS = f'{worknet_scrapper.ADDRESS}?authKey={worknet_scrapper.AUTHKEY}'
         for wanted in self.api_list:
             detail_para={
                 'callTp': CALL_TP,
@@ -61,10 +81,11 @@ class worknet_scrapper:
                 'infoSvc':wanted['infoSvc']
             }
 
-            # get the detailed infomation from api
-            address_detail = worknet_scrapper.get_address(ADDRESS, detail_para)
+            # make addres for API
+            address_detail = worknet_scrapper.get_address(detail_para)
             
             try:
+                # get the detailed infomation from api
                 self.api_detail.append(worknet_scrapper.get_json(address_detail))
                 time.sleep(1)
             except ConnectionRefusedError:
@@ -78,6 +99,10 @@ class worknet_scrapper:
         collection[DATABASE_NAME][COLLECTION_NAME].insert_many(self.api_detail)
     
     def to_json(self, file_name:str):
+        '''
+        scrap_api_detail에서 받아온 리스트를 로컬 json 파일로 저장한다.
+        file_name: 파일 이름. 반드시 .json 파일이어야 한다.
+        '''
         with open(file_name, 'w') as f:
             json.dump(self.api_detail, f, indent=2)
 
